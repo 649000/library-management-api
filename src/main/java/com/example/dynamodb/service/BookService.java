9c8cb8282b4501aa6552a1909a4378bada357c9b
Nazri <649000@users.noreply.github.com>
2021-10-14 13:36:24 +0800

    Updated BookService with CRUD functionality

---
+++ src/main/java/com/example/dynamodb/service/BookService.java
@@ -0,0 +1,73 @@
+package com.example.dynamodb.service;
+
+import com.example.dynamodb.exception.EntityNotFoundException;
+import com.example.dynamodb.model.Author;
+import com.example.dynamodb.model.Book;
+import com.example.dynamodb.repository.AuthorRepository;
+import com.example.dynamodb.repository.BookRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    public Book readBookById(String id) {
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            return book.get();
+        }
+        throw new EntityNotFoundException("Cant find any book under given ID");
+    }
+    public Iterable<Book> readBooks() {
+        return bookRepository.findAll();
+    }
+    public Book readBook(String isbn) {
+        Optional<Book> book = bookRepository.findByIsbn(isbn);
+        if (book.isPresent()) {
+            return book.get();
+        }
+        throw new EntityNotFoundException("Cant find any book under given ISBN");
+    }
+
+    public Book createBook(Book book) {
+        Optional<Author> author = authorRepository.findById(book.getAuthorId());
+        if (!author.isPresent()) {
+            throw new EntityNotFoundException("Author Not Found");
+        }
+        Book bookToCreate = new Book();
+        BeanUtils.copyProperties(book, bookToCreate);
+        bookToCreate.setAuthorId(author.get().getId());
+        return bookRepository.save(bookToCreate);
+    }
+    public void deleteBook(String id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Book updateBook(String bookId, Book request) {
+        Optional<Author> author = authorRepository.findById(request.getAuthorId());
+        if (!author.isPresent()) {
+            throw new EntityNotFoundException("Author Not Found");
+        }
+        Optional<Book> optionalBook = bookRepository.findById(bookId);
+        if (!optionalBook.isPresent()) {
+            throw new EntityNotFoundException("Book Not Found");
+        }
+        Book book = optionalBook.get();
+        book.setIsbn(request.getIsbn());
+        book.setName(request.getName());
+        book.setAuthorId(author.get().getId());
+        return bookRepository.save(book);
+    }
+}
