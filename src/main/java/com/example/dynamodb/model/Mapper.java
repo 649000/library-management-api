bb1215469fb7947979009baa5a34cf1498756243
Nazri <649000@users.noreply.github.com>
2022-11-16 19:31:12 +0800

    Added mapper class

---
+++ src/main/java/com/example/dynamodb/model/Mapper.java
@@ -0,0 +1,19 @@
+package com.example.dynamodb.model;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@org.mapstruct.Mapper(componentModel = "spring")
+public interface Mapper {
+    //Drawback: can't pass null values to database
+    //Alternative is to use @DynamicUpdate
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Author updateAuthor(Author request, @MappingTarget Author author);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Book updateBook(Book request, @MappingTarget Book book);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Member updateMember(Member request, @MappingTarget Member member);
+}
