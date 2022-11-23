884ce4de91ffeb3afae8f77c5f6b125c04c888bd
Nazri <649000@users.noreply.github.com>
2022-11-23 23:29:08 +0800

    Cleaned imports

--- src/main/java/com/example/dynamodb/controller/graphql/AuthorGQLController.java
+++ src/main/java/com/example/dynamodb/controller/graphql/AuthorGQLController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
