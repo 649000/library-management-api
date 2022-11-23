88f1dbd4841fe5e0fb7ea08be9ffd533be1d87e3
Nazri <649000@users.noreply.github.com>
2022-11-23 23:28:44 +0800

    Implementation for author query

--- src/main/java/com/example/dynamodb/controller/graphql/AuthorGQLController.java
+++ src/main/java/com/example/dynamodb/controller/graphql/AuthorGQLController.java
@@ -4,6 +4,7 @@ import com.example.dynamodb.model.Author;
 import com.example.dynamodb.service.AuthorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,10 @@ public class AuthorGQLController {
     @QueryMapping(value = "authors")
     public Iterable<Author> getAllAuthors(){
         return authorService.getAuthors();
+    }
+
+    @QueryMapping(value = "author")
+    public Author getAuthor(@Argument String authorId){
+        return authorService.getAuthor(authorId);
     }
 }
